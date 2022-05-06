e359469a2f472f345facd729e550aad6c1a3ead6
Muhammad Haris <018142muhammadharis@gmail.com>
2022-05-06 22:00:22 +0500

    staking initial commit

---
+++ Contracts/Staking.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+error Staking__TransferFailed();
+error Staking_NeedsMoreThanZero();
+
+contract Staking {
+
+    IERC20 public s_stakingToken;
+    IERC20 public s_rewardsToken;
+
+    uint256 public s_totalSupply;
+
+    uint256 public s_rewardPerTokenStored;
+
+    uint256 public s_lastUpdateTime;
+
+    uint256 public constant REWARD_RATE = 100;
+
+    mapping(address=>uint256) public s_balances;
+
+    mapping(address=>uint256) public s_userRewardPerTokenPaid;
+
+    mapping(address=>uint256) public s_rewards;
+
+    modifier updateReward(address account){
+        s_rewardPerTokenStored = rewardPersToken();
+        s_lastUpdateTime = block.timestamp;
+        s_rewards[account] = earned(account);
+        s_userRewardPerTokenPaid[account] = s_rewardPerTokenStored;
+        _;
+    }
+
+    modifier moreThanZero(uint256 amount){
+        if(amount == 0){
+            revert Staking_NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    constructor(address stakingToken, address rewardToken){
+        s_stakingToken = IERC20(stakingToken);
+        s_rewardsToken = IERC20(rewardToken);
+    }
+
+    function earned(address account) public view returns(uint256) {
+        uint256 currentBalance = s_balances[account];
+        uint256 amountPaid = s_userRewardPerTokenPaid[account];
+        uint256 currentRewardPerToken = rewardPersToken();
+        uint256 pastRewards = s_rewards[account];
+
+        uint256 _earned = ((currentBalance *(currentRewardPerToken-amountPaid))/1e18) + pastRewards;
+        return _earned;
+    }
+
+    function rewardPersToken() public view returns (uint256){
+        if(s_totalSupply == 0){
+            return s_rewardPerTokenStored;
+        }
+        return s_rewardPerTokenStored + (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18)/s_totalSupply);
+    }
+
+    function stake(uint256 amount) external updateReward(msg.sender) moreThanZero(amount){
+        s_balances[msg.sender] = s_balances[msg.sender] +
+        amount;
+        s_totalSupply = s_totalSupply + amount;
+        bool success = s_stakingToken.transferFrom(msg.sender, address(this), amount);
+        if(!success){
+            revert Staking__TransferFailed();
+        }
+        // require(success,"failed!");
+    }
+
+    function withdraw(uint256 amount) external updateReward(msg.sender) moreThanZero(amount){
+        s_balances[msg.sender] = s_balances[msg.sender] - amount;
+        s_totalSupply = s_totalSupply - amount;
+        bool success = s_stakingToken.transfer(msg.sender, amount);
+        // bool success = s_stakingToken.transferFrom(address(this), msg.sender, amount);
+        if(!success){
+            revert Staking__TransferFailed();
+        }
+    }
+
+    function claimRewards() external updateReward(msg.sender){
+        uint256 reward = s_rewards[msg.sender];
+        bool success = s_rewardsToken.transfer(msg.sender, reward);
+        if(!success){
+            revert Staking__TransferFailed();
+        }
+    }
+}
